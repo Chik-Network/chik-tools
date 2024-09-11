3e9f79d7bde6cddd901f7d92391a4344161d0432
Chris Marslender <chrismarslender@gmail.com>
2024-09-11 10:50:29 -0500

    Add optional --ca arg to use an existing private CA when generating certs (#16)
    * Add optional --ca arg to use an existing private CA when generating certs
    
    * Add a check for any other errors

--- cmd/certs/generate.go
+++ cmd/certs/generate.go
@@ -1,6 +1,12 @@
 package certs
 
 import (
+	"crypto/rsa"
+	"crypto/x509"
+	"errors"
+	"os"
+	"path"
+
 	"github.com/chia-network/go-chia-libs/pkg/tls"
 	"github.com/chia-network/go-modules/pkg/slogs"
 	"github.com/spf13/cobra"
@@ -13,7 +19,48 @@ var generateCmd = &cobra.Command{
 	Short:   "Generates a full set of certificates for chia-blockchain",
 	Example: "chia-tools certs generate --output ~/.chia/mainnet/config/ssl",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := tls.GenerateAllCerts(viper.GetString("cert-output"))
+		var privateCACert *x509.Certificate
+		var privateCAKey *rsa.PrivateKey
+		caDir := viper.GetString("ca")
+		if caDir != "" {
+			caCertPath := path.Join(caDir, "private_ca.crt")
+			caKeyPath := path.Join(caDir, "private_ca.key")
+
+			if _, err := os.Stat(caCertPath); err != nil {
+				if errors.Is(err, os.ErrNotExist) {
+					slogs.Logr.Fatal("private_ca.crt does not exist at the provided path", "path", caCertPath)
+				} else {
+					slogs.Logr.Fatal("error checking private_ca.crt", "error", err)
+				}
+			}
+
+			certBytes, err := os.ReadFile(caCertPath)
+			if err != nil {
+				slogs.Logr.Fatal("error reading ca cert from filesystem", "error", err)
+			}
+			privateCACert, err = tls.ParsePemCertificate(certBytes)
+			if err != nil {
+				slogs.Logr.Fatal("error parsing certificate", "error", err)
+			}
+
+			if _, err := os.Stat(caKeyPath); err != nil {
+				if errors.Is(err, os.ErrNotExist) {
+					slogs.Logr.Fatal("private_ca.key does not exist at the provided path", "path", caKeyPath)
+				} else {
+					slogs.Logr.Fatal("error checking private_ca.key", "error", err)
+				}
+			}
+
+			keyBytes, err := os.ReadFile(caKeyPath)
+			if err != nil {
+				slogs.Logr.Fatal("error reading ca key from filesystem", "error", err)
+			}
+			privateCAKey, err = tls.ParsePemKey(keyBytes)
+			if err != nil {
+				slogs.Logr.Fatal("error parsing key", "error", err)
+			}
+		}
+		err := tls.GenerateAllCerts(viper.GetString("cert-output"), privateCACert, privateCAKey)
 		if err != nil {
 			slogs.Logr.Fatal("error generating certificates", "error", err)
 		}
@@ -21,7 +68,10 @@ var generateCmd = &cobra.Command{
 }
 
 func init() {
+	generateCmd.PersistentFlags().String("ca", "", "Optionally specify a directory that has an existing private_ca.crt/key")
 	generateCmd.PersistentFlags().StringP("output", "o", "certs", "Output directory for certs")
+
+	cobra.CheckErr(viper.BindPFlag("ca", generateCmd.PersistentFlags().Lookup("ca")))
 	cobra.CheckErr(viper.BindPFlag("cert-output", generateCmd.PersistentFlags().Lookup("output")))
 
 	certsCmd.AddCommand(generateCmd)
