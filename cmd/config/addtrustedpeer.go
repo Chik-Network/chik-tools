defa12c2027dc3f95951b56a50e155c32c8b5e93
Chik-Network <Chik-Network@users.noreply.github.com>
2025-06-17 16:18:42 +0800

    update 0.12.7

--- cmd/config/addtrustedpeer.go
+++ cmd/config/addtrustedpeer.go
@@ -26,7 +26,11 @@ var addTrustedPeerCmd = &cobra.Command{
 	Example: `chik-tools config add-trusted-peer 1.2.3.4
 
 # The following version will also override the port to use when connecting to this peer
-chik-tools config add-trusted-peer 1.2.3.4 19678`,
+chik-tools config add-trusted-peer 1.2.3.4 19678
+
+# You may also specify a DNS name. The tool will attempt to resolve the name to an IP address.
+# If the name resolves to multiple IP addresses, chik-tools will attempt to connect to each one to add it to the config.
+chik-tools config add-trusted-peer node.chiknetwork.com 9678`,
 	Run: func(cmd *cobra.Command, args []string) {
 		chikRoot, err := config.GetChikRootPath()
 		if err != nil {
@@ -60,62 +64,81 @@ chik-tools config add-trusted-peer 1.2.3.4 19678`,
 			port = uint16(port64)
 		}
 
+		var ips []net.IP
+
 		ip := net.ParseIP(peer)
 		if ip == nil {
-			slogs.Logr.Fatal("Invalid IP address", "id", peer)
-		}
-		slogs.Logr.Info("Attempting to get peer id", "peer", peer, "port", port)
-
-		keypair, err := cfg.FullNode.SSL.LoadPublicKeyPair(chikRoot)
-		if err != nil {
-			slogs.Logr.Fatal("Error loading certs from CHIK_ROOT", "CHIK_ROOT", chikRoot, "error", err)
-		}
-		if keypair == nil {
-			slogs.Logr.Fatal("Error loading certs from CHIK_ROOT", "CHIK_ROOT", chikRoot, "error", "keypair was nil")
-		}
-		conn, err := peerprotocol.NewConnection(
-			&ip,
-			peerprotocol.WithPeerPort(port),
-			peerprotocol.WithNetworkID(*cfg.SelectedNetwork),
-			peerprotocol.WithPeerKeyPair(*keypair),
-		)
-		if err != nil {
-			slogs.Logr.Fatal("Error creating connection", "error", err)
-		}
-		peerID, err := conn.PeerID()
-		if err != nil {
-			slogs.Logr.Fatal("Error getting peer id", "error", err)
-		}
-		peerIDStr := hex.EncodeToString(peerID[:])
-		slogs.Logr.Info("peer id received", "peer", peerIDStr)
-		if !utils.ConfirmAction("Would you like trust this peer? (y/N)", skipConfirm) {
-			slogs.Logr.Error("Cancelled")
-		}
-		cfg.Wallet.TrustedPeers[peerIDStr] = "Does_not_matter"
-
-		peerToAdd := config.Peer{
-			Host: ip.String(),
-			Port: port,
-		}
-
-		foundPeer := false
-		for idx, peer := range cfg.Wallet.FullNodePeers {
-			if peer.Host == ip.String() {
-				foundPeer = true
-				cfg.Wallet.FullNodePeers[idx] = peerToAdd
+			// Try to resolve a DNS name
+			ips, err = net.LookupIP(peer)
+			if err != nil {
+				slogs.Logr.Fatal("Couldn't parse peer as IP address or resolve to a host", "id", peer)
 			}
-		}
-		if !foundPeer {
-			cfg.Wallet.FullNodePeers = append(cfg.Wallet.FullNodePeers, peerToAdd)
+			if len(ips) == 0 {
+				slogs.Logr.Fatal("dns lookup returned 0 IPs ", "id", peer)
+			}
+		} else {
+			ips = append(ips, ip)
 		}
 
-		err = cfg.Save()
-		if err != nil {
-			slogs.Logr.Fatal("error saving config", "error", err)
+		for _, ip := range ips {
+			addTrustedPeer(cfg, chikRoot, ip, port)
 		}
 
 		slogs.Logr.Info("Added trusted peer. Restart your chik services for the configuration to take effect")
 	},
+}
+
+func addTrustedPeer(cfg *config.ChikConfig, chikRoot string, ip net.IP, port uint16) {
+	slogs.Logr.Info("Attempting to get peer id", "peer", ip.String(), "port", port)
+
+	keypair, err := cfg.FullNode.SSL.LoadPublicKeyPair(chikRoot)
+	if err != nil {
+		slogs.Logr.Fatal("Error loading certs from CHIK_ROOT", "CHIK_ROOT", chikRoot, "error", err)
+	}
+	if keypair == nil {
+		slogs.Logr.Fatal("Error loading certs from CHIK_ROOT", "CHIK_ROOT", chikRoot, "error", "keypair was nil")
+	}
+	conn, err := peerprotocol.NewConnection(
+		&ip,
+		peerprotocol.WithPeerPort(port),
+		peerprotocol.WithNetworkID(*cfg.SelectedNetwork),
+		peerprotocol.WithPeerKeyPair(*keypair),
+	)
+	if err != nil {
+		slogs.Logr.Fatal("Error creating connection", "error", err)
+	}
+	peerID, err := conn.PeerID()
+	if err != nil {
+		slogs.Logr.Fatal("Error getting peer id", "error", err)
+	}
+	peerIDStr := hex.EncodeToString(peerID[:])
+	slogs.Logr.Info("peer id received", "peer", peerIDStr)
+	if !utils.ConfirmAction("Would you like trust this peer? (y/N)", skipConfirm) {
+		slogs.Logr.Error("Cancelled")
+		return
+	}
+	cfg.Wallet.TrustedPeers[peerIDStr] = "Does_not_matter"
+
+	peerToAdd := config.Peer{
+		Host: ip.String(),
+		Port: port,
+	}
+
+	foundPeer := false
+	for idx, peer := range cfg.Wallet.FullNodePeers {
+		if peer.Host == ip.String() {
+			foundPeer = true
+			cfg.Wallet.FullNodePeers[idx] = peerToAdd
+		}
+	}
+	if !foundPeer {
+		cfg.Wallet.FullNodePeers = append(cfg.Wallet.FullNodePeers, peerToAdd)
+	}
+
+	err = cfg.Save()
+	if err != nil {
+		slogs.Logr.Fatal("error saving config", "error", err)
+	}
 }
 
 func init() {
